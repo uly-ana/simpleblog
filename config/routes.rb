69819d7f51667c514b8cfa7f02f87c7b9635a245
Ulyana Melnyk <ulianamelnyk@gmail.com>
2019-05-26 22:14:28 +0800

    validations added: title must contain at least 1 symbol, body text min 5 characters. Error msg appear

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'posts#index'
-  get 'about' => 'pages#about'
+  root 'posts#index', as: 'home'
+  get 'about' => 'pages#about', as: 'about'
 
   resources :posts
 end
